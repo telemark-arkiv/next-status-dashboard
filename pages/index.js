7f50350cffdd061385770ea4277874f18f47a1fb
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-08 21:59:39 +0100

    Standard fixes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Queue from '../components/queue'
 export default () => (
   <div>
     <h1>Hello, dashboard!</h1>
-    <img src="/static/images/highestoffives.gif" />
+    <img src='/static/images/highestoffives.gif' />
     <Queue />
   </div>
 
