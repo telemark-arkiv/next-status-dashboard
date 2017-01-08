d30a007228da69328d60abb273ee6470dd5226a8
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-08 21:58:23 +0100

    Initial Queue

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 'use strict'
 
+import Queue from '../components/queue'
+
 export default () => (
   <div>
     <h1>Hello, dashboard!</h1>
     <img src="/static/images/highestoffives.gif" />
+    <Queue />
   </div>
+
 )
