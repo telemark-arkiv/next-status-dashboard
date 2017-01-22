861f611b4fdffda4fd44c8b01c7b7d488f585aff
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-22 16:29:07 +0100

    Moves data collection to component

--- components/status.js
+++ components/status.js
@@ -1,14 +1,28 @@
 'use strict'
 
 import React from 'react'
+const getData = require('../lib/get-data')
 
 export default class Status extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  async componentDidMount () {
+    const data = await getData(this.props.source)
+    this.setState({data: data})
+  }
+
   render () {
     return (
       <div>
         <h2>{this.props.title}</h2>
         <ul>
-          {this.props.data.map((line) => {
+          {this.state.data.map((line) => {
             return (
               <li>{line.name}: {line.status}</li>
             )
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 'use strict'
 
 import React from 'react'
-import collectData from '../lib/collect-data'
 import Status from '../components/status'
 import Timer from '../components/timer'
 
 export default class Index extends React.Component {
-  static async getInitialProps (req) {
-    const data = await collectData()
-    return data
-  }
 
   render () {
     return (
       <div>
         <h1>Statusoversikt</h1>
-        <Timer title="Timer spart" data={this.props.timer} />
-        <Status title="Køer" data={this.props.queue} />
-        <Status title="Antall" data={this.props.antall} />
+        <Timer title='Timer spart' source='https://cors.allthethings.win/https://timer.status.t-fk.win/json' />
+        <Status title='Køer' source='https://cors.allthethings.win/https://queue.status.t-fk.win/json' />
+        <Status title='Antall' source='https://cors.allthethings.win/https://antall.status.t-fk.win/json' />
       </div>
     )
   }
