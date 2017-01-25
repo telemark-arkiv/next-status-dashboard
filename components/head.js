cd28c1e925257bd377f19b2baa2638d5df890880
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-25 19:15:00 +0100

    Adds mui

---
+++ components/head.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Dashboard digitalisering</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link href='//cdn.muicss.com/mui-0.9.9-rc2/css/mui.min.css' rel='stylesheet' type='text/css' media='screen' />
+    </Head>
+  </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 'use strict'
 
 import React from 'react'
+import Container from 'muicss/lib/react/container'
+import Row from 'muicss/lib/react/row'
+import Col from 'muicss/lib/react/col'
+import Head from '../components/head'
 import Status from '../components/status'
 import Timer from '../components/timer'
 
@@ -9,11 +13,26 @@ export default class Index extends React.Component {
   render () {
     return (
       <div>
-        <h1>Statusoversikt</h1>
-        <Timer title='Timer spart' source='https://timer.status.t-fk.win/json' />
-        <Status title='Køer' source='https://queue.status.t-fk.win/json' />
-        <Status title='Antall' source='https://antall.status.t-fk.win/json' />
-        <Status title='vigo' source='https://vigo.saksbehandling.status.t-fk.win/json' />
+        <Head />
+        <Container fluid>
+          <h1 className='mui--text-display3'>Statusoversikt</h1>
+          <Row>
+            <Col md='4'>
+              <Timer title='Timer spart' source='https://timer.status.t-fk.win/json' />
+            </Col>
+            <Col md='4'>
+              <Status title='Køer' source='https://queue.status.t-fk.win/json' />
+            </Col>
+            <Col md='4'>
+              <Status title='Antall' source='https://antall.status.t-fk.win/json' />
+            </Col>
+          </Row>
+          <Row>
+            <Col md='4'>
+              <Status title='vigo' source='https://vigo.saksbehandling.status.t-fk.win/json' />
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
