05e3eff1b3f5af84f339863971e5b83c6d2b00cd
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-25 22:05:21 +0100

    Moar dummies

--- pages/index.js
+++ pages/index.js
@@ -17,28 +17,31 @@ export default class Index extends React.Component {
         <Container fluid>
           <h1 className='mui--text-display3'>Statusoversikt</h1>
           <Row>
-            <Col md='4'>
+            <Col md='6'>
               <Timer title='Timer spart' source='https://timer.status.t-fk.win/json' />
             </Col>
-            <Col md='4'>
-              <Status title='Køer' source='https://queue.status.t-fk.win/json' />
-            </Col>
-            <Col md='4'>
+            <Col md='6'>
               <Status title='Antall' source='https://antall.status.t-fk.win/json' />
             </Col>
           </Row>
           <Row>
-            <Col md='3'>
+            <Col md='2'>
+              <Status title='Køer' source='https://queue.status.t-fk.win/json' />
+            </Col>
+            <Col md='2'>
               <Status title='rambo' source='https://vigo.saksbehandling.status.t-fk.win/json' />
             </Col>
-            <Col md='3'>
+            <Col md='2'>
               <Status title='minelev' source='https://vigo.saksbehandling.status.t-fk.win/json' />
             </Col>
-            <Col md='3'>
+            <Col md='2'>
               <Status title='skoleskyss' source='https://vigo.saksbehandling.status.t-fk.win/json' />
             </Col>
-            <Col md='3'>
+            <Col md='2'>
               <Status title='tilskudd' source='https://vigo.saksbehandling.status.t-fk.win/json' />
+            </Col>
+            <Col md='2'>
+              <Status title='SvarUT' source='https://vigo.saksbehandling.status.t-fk.win/json' />
             </Col>
           </Row>
         </Container>
