6603b5c01f37e67af71183b0f58dbf14e7a019f3
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-31 20:22:02 +0100

    Fixes url

--- config/urls.js
+++ config/urls.js
@@ -1,5 +1,5 @@
 'use strict'
 
 module.exports = {
-  svarUt: 'https://universal.status.t-fk.win/json?url=https://seneca-firebase-test.firebaseio.com/tfk-distribusjon-svarut.json?fields=jobs|errors|done|retries?title=SvarUT'
+  svarUt: 'https://universal.status.t-fk.win/json?url=https://seneca-firebase-test.firebaseio.com/tfk-distribusjon-svarut.json&fields=jobs|errors|done|retries&title=SvarUT'
 }
